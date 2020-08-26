01b7831f3e0219a08b22779953deba9c634a6656
yortuc <yortucboylu@gmail.com>
2020-08-26 07:45:48 +0200

    parse ast

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 class Editor{
   constructor(){
+    this.codeFunc = null;
     this.DONT_REFRESH = false;
 
     const value = "// ctx is injected\n\nctx.fillRect(0, 0, 100, 100);\n\n";
@@ -21,13 +22,14 @@ class Editor{
   }
 
   onDidChangeCursorPosition(e){
-    console.log(e);
-    highlightCanvas(this.drawingMapper.calls, e.position.lineNumber);
+    // code -> drawing mapping
+    this.highlightCanvas(e.position.lineNumber);
   }
 
   onDidChangeContent(){
     const code = this.model.getValue();
-    this.runCode(code);
+    this.updateCode(code);
+    this.executeCode();
 
     if(!this.DONT_REFRESH){
       this.saveCode(code);
@@ -37,7 +39,7 @@ class Editor{
   saveCode(code){
     localStorage.setItem('cclab_code', code);
 
-    const viewState = editor.saveViewState();
+    const viewState = this.editor.saveViewState();
     localStorage.setItem('cclab_viewstate', JSON.stringify(viewState))
   }
 
@@ -45,23 +47,45 @@ class Editor{
     return localStorage.getItem('cclab_code')
   }
 
-  draw(){
-    this.runCode(this.model.getValue());
+  executeCode(){
+    context.clearRect(0, 0, 800, 600);
+    
+    try{
+      this.drawingMapper.calls = [];
+      this.codeFunc(this.drawingMapper.getCtx())
+      document.getElementById("indicator").style.backgroundColor = "rgb(0,230,23)";
+      console.log(this.drawingMapper.calls);
+    }
+    catch (error){
+      const err = extractErrorInfo(error);
+      console.log(err);
+      document.getElementById("indicator").style.backgroundColor = "rgb(243, 12,34)";
+    }
   }
 
-  runCode(code){
-    try{
-        const funcCreateObj = new Function("ctx", code)
-        this.drawingMapper = new DrawingMapper()
-        const ctx = this.drawingMapper.getCtx()
-        const obj = funcCreateObj(ctx)
-        document.getElementById("indicator").style.backgroundColor = "rgb(0,230,23)";
+  highlightCanvas(curLine){
+    const mappings = this.drawingMapper.calls;
+
+    context.clearRect(0, 0, 800, 600);
+    this.executeCode();
+
+    context.save()
+    context.strokeStyle = "red";
+    context.lineWidth = 3;
+
+    for(let i=0; i<mappings.length; i++){
+      let m = mappings[i];
+      if(m.lineNumber == curLine){
+        context.strokeRect(m.x, m.y, m.w, m.h)
       }
-      catch (error){
-        const err = extractErrorInfo(error);
-        console.log(err);
-        document.getElementById("indicator").style.backgroundColor = "rgb(243, 12,34)";
-      }
+    }
+    context.restore();
+
+  }
+
+  updateCode(code){
+    this.codeFunc = new Function("ctx", code)
+    this.drawingMapper = new DrawingMapper()
   }
 
   start(){
@@ -70,7 +94,9 @@ class Editor{
       this.DONT_REFRESH = true;
       this.model.setValue(savedCode);
       this.DONT_REFRESH = false;
-        
+      
+      this.updateCode(savedCode);
+
         const viewState = localStorage.getItem('cclab_viewstate');
         if (viewState){
           this.editor.restoreViewState(JSON.parse(viewState));
@@ -92,15 +118,26 @@ class DrawingMapper{
     // all the ctx api has to be wrapped :\
     return {
       fillRect: (x,y,w,h) => {
+        const err = new Error();
         this.calls.push({
-          lineNumber: this.getLine(new Error()),
+          caller: this.getCaller(err),
+          lineNumber: this.getLine(err),
           x,y,w,h
         })
 
         context.fillRect(x,y,w,h)
-        console.log(this.calls)
       }
     }
+  }
+  getCaller(err){
+    var stackTrace = (new Error()).stack; // Only tested in latest FF and Chrome
+    var callerName = stackTrace.replace(/^Error\s+/, ''); // Sanitize Chrome
+    callerName = callerName.split("\n")[1]; // 1st item is this, 2nd item is caller
+    callerName = callerName.replace(/^\s+at Object./, ''); // Sanitize Chrome
+    callerName = callerName.replace(/ \(.+\)$/, ''); // Sanitize Chrome
+    callerName = callerName.replace(/\@.+/, ''); // Sanitize Firefox
+
+    return callerName
   }
   getLine(err){
     const start = err.stack.indexOf("<anonymous>:")
@@ -126,24 +163,3 @@ function extractErrorInfo(err){
 const editor = new Editor()
 
 editor.start();
-
-function highlightCanvas(mappings, curLine){
-  context.clearRect(0, 0, 800, 600);
-  editor.draw();
-
-  const curMaps = mappings.filter(m => m.lineNumber == curLine)
-  
-  console.log(curMaps.length)
-
-  curMaps.forEach(m => {
-    console.log(m);
-    context.save()
-    context.strokeStyle = "red";
-    context.lineWidth = 3;
-
-    context.rect(m.x, m.y, m.w, m.h)
-    context.stroke()
-    context.restore()
-  });
-
-}
